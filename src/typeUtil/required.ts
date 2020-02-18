6aadbf1b75897ac53ddb3f0e51767dc802696c62
inter727 <inter727@163.com>
2020-02-18 12:38:34 +0800

    添加类型工具注释2

--- src/typeUtil/required.ts
+++ src/typeUtil/required.ts
@@ -1,5 +1,27 @@
+/**
+ * Required
+ * @description T的属性都改为必选
+ * @example
+ * // Expect {name: string, age: number}
+ * Required<{name:string, age?: number}>
+ */
 export type Required<T> = {[K in keyof T]-?: T[K]}
 
+/**
+ * DeepRequired
+ * @description T的属性都改为必选，包括属性的属性
+ * @example
+ * interface IProject {
+ *  name: string,
+ *  member?: {name: string, age?: number}
+ * }
+ *
+ * // Expect: {
+ *   name: string,
+ *   member: {name: string, age: number}
+ * }
+ * DeepRequired<IProject>
+ */
 export type DeepRequired<T> = {
   [K in keyof T]-?: T[K] extends object
     ? DeepRequired<T[K]>
--- src/typeUtil/returnType.ts
+++ src/typeUtil/returnType.ts
@@ -1,1 +1,8 @@
+/**
+ * ReturnType
+ * @description 取出T的返回类型
+ * @example
+ * // Expect: number
+ * ReturnType<(param: string) => number>
+ */
 export type ReturnType<T> = T extends (...args: any[]) => infer P ? P : T
